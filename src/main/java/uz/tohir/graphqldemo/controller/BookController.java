414cc3da1c5cff7fc71418a9dde015876930b02a
Bismillah <guvalakat1603@gmail.con>
2022-10-01 16:34:46 +0500

    add controllers

---
+++ src/main/java/uz/tohir/graphqldemo/controller/BookController.java
@@ -0,0 +1,30 @@
+package uz.tohir.graphqldemo.controller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import uz.tohir.graphqldemo.model.Book;
+import uz.tohir.graphqldemo.repository.BookRepository;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @SchemaMapping(typeName = "Query",value = "allBooks")
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @QueryMapping
+    public Book findOne(@Argument Integer id) {
+        return bookRepository.findOne(id);
+    }
+}
